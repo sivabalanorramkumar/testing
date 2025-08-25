ea71f2c314836982c49b86bd3a6d4af940a19c29
sivabalanorramkumar <dsbtechtamil@gmail.com>
2025-08-25 11:40:21 -0700

    toy program

---
+++ Js/basics/Object in js/Object.js
@@ -0,0 +1,17 @@
+//creating object   
+let person = {
+    name: "Siva",
+    age:56,
+    native:"Chennai",
+    Haircolor:"Black"
+}
+
+
+//declaer the empty object 
+let Name = {}
+
+//assign the value for the object 
+Name.yourName ="Siva";
+
+
+console.log(Name)
